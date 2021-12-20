68aa6d182d02960bdc43abfc800cf19396474cbc
jwyoon12 <30345923+jwyoon12@users.noreply.github.com>
2021-12-20 15:29:17 +0900

    Add directory to save generate text

--- score.sh
+++ score.sh
@@ -3,4 +3,4 @@ export lang2="je"
 
 fairseq-generate data/4k/${lang1}-${lang2}-bin \
   --path train/4k/${lang1}-${lang2}/ckpt/checkpoint_best.pt \
-  --batch-size 200 --beam 5
+  --batch-size 200 --results-path result/4k/${lang1}-${lang2}-bin --beam 5
