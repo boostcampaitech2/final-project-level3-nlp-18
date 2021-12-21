a8712643b01f6c12e670984ba79d71d3bdce24c9
jwyoon12 <30345923+jwyoon12@users.noreply.github.com>
2021-12-21 17:03:34 +0900

    Update and rename eval.sh to interactive.sh

--- interactive.sh
+++ interactive.sh
@@ -3,4 +3,5 @@ export lang2="je"
 
 fairseq-interactive data/4k/${lang1}-${lang2}-bin  \
   --path train/4k/${lang1}-${lang2}/ckpt/checkpoint_best.pt \
-  --beam 5 
+  --remove-bpe \
+  --results-path interactive_result/4k/${lang1}-${lang2}-bin --beam 5
