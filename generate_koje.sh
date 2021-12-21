ae39ddde031e261abdcf6d958d86ab685f3997b5
jwyoon12 <30345923+jwyoon12@users.noreply.github.com>
2021-12-21 16:50:51 +0900

    Update generate_koje.sh

--- generate_koje.sh
+++ generate_koje.sh
@@ -5,4 +5,4 @@ fairseq-generate data/4k/${lang1}-${lang2}-bin \
   --path train/4k/${lang1}-${lang2}/ckpt/checkpoint_best.pt \
   --batch-size 200 \
   --remove-bpe \
-  --beam 5
+  --results-path result/4k/${lang1}-${lang2}-bin --beam 5
