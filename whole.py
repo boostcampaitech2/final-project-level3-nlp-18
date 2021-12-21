34ff61fbd61960d1a0c4f887dbf9f369c1395754
jwyoon12 <30345923+jwyoon12@users.noreply.github.com>
2021-12-21 16:47:06 +0900

    Update whole.py

--- whole.py
+++ whole.py
@@ -6,7 +6,7 @@ ko2je = TransformerModel.from_pretrained(
     checkpoint_file = 'checkpoint_best.pt',
     data_name_or_path = 'data/4k/ko-je-bin',
     bpe = 'subword_nmt',
-    sentencepiece.bpe.model = 'data/4k/bpe/bpe.model'
+    bep_codes = ?? (bpe에 관한 code파일인거 같은데?? 못 찾았어요)
     
 )
 ko2je.translate('안녕하세요. 반갑습니다')
