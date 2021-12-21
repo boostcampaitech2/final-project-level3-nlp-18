16e1459c659fd1d13233684341b05e688ed10412
jwyoon12 <30345923+jwyoon12@users.noreply.github.com>
2021-12-21 16:37:17 +0900

    Update score.sh

--- score.sh
+++ score.sh
@@ -1,6 +1,5 @@
-export lang1="ko"
-export lang2="je"
+export lang1='ko'
+export lang2='je'
 
-fairseq-generate data/4k/${lang1}-${lang2}-bin \
+fairseq-score data/4k/${lang1}-${lang2}-bin  \
   --path train/4k/${lang1}-${lang2}/ckpt/checkpoint_best.pt \
-  --batch-size 200 --results-path result/4k/${lang1}-${lang2}-bin --beam 5
