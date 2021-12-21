90e04bc0f2ead00920b118d872ca96149abca4cc
jwyoon12 <30345923+jwyoon12@users.noreply.github.com>
2021-12-22 00:19:34 +0900

    Update whole.py

--- whole.py
+++ whole.py
@@ -5,8 +5,8 @@ ko2je = TransformerModel.from_pretrained(
     'train/4k/ko-je/ckpt',
     checkpoint_file = 'checkpoint_best.pt',
     data_name_or_path = 'data/4k/ko-je-bin',
-    bpe = 'subword_nmt',
-    bep_codes = ?? (bpe에 관한 code파일인거 같은데?? 못 찾았어요)
+    bpe = 'sentencepiece',
+    bpe_codes='train/4k/ko-je/ckpt/sentencepiece.bpe.model'
     
 )
-ko2je.translate('안녕하세요. 반갑습니다')
+print(ko2je.translate('명절 때는 더 온다 세뱃돈 받을려고'))
