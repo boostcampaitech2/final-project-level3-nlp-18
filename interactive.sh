c866c294006158c1153cf631e0498ee6e9a9fc60
jwyoon12 <30345923+jwyoon12@users.noreply.github.com>
2021-12-22 18:58:03 +0900

    Delete interactive.sh

--- interactive.sh
+++
@@ -1,7 +0,0 @@
-export lang1="ko"
-export lang2="je"
-
-fairseq-interactive data/4k/${lang1}-${lang2}-bin  \
-  --path train/4k/${lang1}-${lang2}/ckpt/checkpoint_best.pt \
-  --remove-bpe \
-  --results-path interactive_result/4k/${lang1}-${lang2}-bin --beam 5
