b909803e499648353caa7a91f2b1e2883c74cbc3
jwyoon12 <30345923+jwyoon12@users.noreply.github.com>
2021-12-23 14:00:34 +0900

    Update prepro.py

--- prepro.py
+++ prepro.py
@@ -4,7 +4,20 @@ import argparse
 def prepro(src, tgt, vocab_size):
     dir = "data/{}k".format(str(vocab_size)[:-3])
     destdir = f"{dir}/{src}-{tgt}-bin"
-    if src == 'external' : 
+    if src != 'external' : 
+        trainpref = f"{dir}/bpe/train"
+        prepro = f"fairseq-preprocess \
+                    --source-lang {src} \
+                    --target-lang {tgt} \
+                    --trainpref {trainpref} \
+                    --validpref {dir}/bpe/dev \
+                    --testpref {dir}/bpe/test \
+                    --srcdict {dir}/bpe/bpe.dict \
+                    --tgtdict {dir}/bpe/bpe.dict \
+                    --workers 8 \
+                    --destdir {destdir}"     
+
+    else :     
         trainpref = f'{dir}/bpe_ex/train'
         prepro = f"fairseq-preprocess \
                     --source-lang {src} \
@@ -16,18 +29,7 @@ def prepro(src, tgt, vocab_size):
                     --tgtdict {dir}/bpe_ex/bpe.dict \
                     --workers 8 \
                     --destdir {destdir}"
-    else :       
-            trainpref = f"{dir}/bpe/train"
-            prepro = f"fairseq-preprocess \
-                --source-lang {src} \
-                --target-lang {tgt} \
-                --trainpref {trainpref} \
-                --validpref {dir}/bpe/dev \
-                --testpref {dir}/bpe/test \
-                --srcdict {dir}/bpe/bpe.dict \
-                --tgtdict {dir}/bpe/bpe.dict \
-                --workers 8 \
-                --destdir {destdir}"                                                    
+                                                                   
     os.system(prepro)
 
 
