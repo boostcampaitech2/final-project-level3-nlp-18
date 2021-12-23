619130916100492d882a16491e42cd20f50cc31a
jwyoon12 <30345923+jwyoon12@users.noreply.github.com>
2021-12-23 15:04:26 +0900

    Update generate_ex2je.sh

--- generate_ex2je.sh
+++ generate_ex2je.sh
@@ -2,7 +2,7 @@ export lang1="external"
 export lang2="je"
 
 fairseq-interactive data/4k/${lang1}-${lang2}-bin  \
-  --input jit/exteranl.train \
+  --input jit/external.train \
   --path train/4k/ko-je/ckpt/checkpoint_best.pt \
   --buffer-size 16 \
   --results-path result/4k/${lang1}-${lang2}-bin --beam 5 > backtranslation_output.txt
