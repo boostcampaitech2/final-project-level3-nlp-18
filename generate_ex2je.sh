f2fb985b331be2295d1df2eb2e751a73282b605c
jwyoon12 <30345923+jwyoon12@users.noreply.github.com>
2021-12-23 22:34:58 +0900

    Delete generate_ex2je.sh

--- generate_ex2je.sh
+++
@@ -1,8 +0,0 @@
-export lang1="external"
-export lang2="je"
-
-fairseq-interactive data/4k/${lang1}-${lang2}-bin  \
-  --input jit/external.train \
-  --path train/4k/ko-je/ckpt/checkpoint_best.pt \
-  --buffer-size 16 \
-  --results-path result/4k/${lang1}-${lang2}-bin --beam 5 > backtranslation_output.txt
