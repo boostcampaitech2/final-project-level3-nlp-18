55366d100129ca0636d0e9439c1b0e0291d8ad4e
paekunkyoung <paekunkyoung@gmail.com>
2021-12-23 21:58:18 +0900

    Thu Dec 23 21:58 KST

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-from translator import interactive_one
+from translator import interactive_one, interactive_two
 
 app = Flask(__name__)
 
@@ -18,6 +18,25 @@ def index_post():
         translated_text=translated_text
     )
 
+@app.route('/second', methods=['GET'])
+def second():
+    return render_template('second.html')
+
+@app.route('/second', methods=['POST'])
+def second_post():
+    original_text = request.form['text']
+    translated_text = interactive_two.cli_main(original_text)
+
+    return render_template(
+        'result_second.html',
+        original_text=original_text,
+        translated_text=translated_text
+    )
+
 @app.route('/', methods=['GET'])
 def result():
     return render_template('result.html')
+
+@app.route('/second', methods=['GET'])
+def result_second():
+    return render_template('result_second.html')
