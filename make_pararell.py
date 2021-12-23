6cd7480a85f4d4c293df632bf6ebcf17b15c838a
jwyoon12 <30345923+jwyoon12@users.noreply.github.com>
2021-12-24 01:52:48 +0900

    Update make_pararell.py

--- make_pararell.py
+++ make_pararell.py
@@ -12,7 +12,7 @@ if __name__ == '__main__' :
     with open('backtranslation_output.txt', 'r', encoding='utf-8') as f :
         for line in f : 
             line_list = line.split()
-            if line_list[0][0] == 'D' : 
+            if line_list[0][0] == '제' : 
                 list.append(line_list[2:])
     add_generated_sentence(list)
     
